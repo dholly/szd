70cd515dfaef3438b9388c763572823e6f960b4e
Vasiliy <boosting007@gmail.com>
2022-05-20 07:21:28 +0300

    'Done'

--- src/js/index.js
+++ src/js/index.js
@@ -13,21 +13,66 @@ const app = new Vue({
     el:'#app'
 }); */
 
-import { createApp } from 'vue';
+import {createApp} from 'vue';
+
 import test from './components/carousel.vue';
+
 const app = createApp(test);
-console.log('1',app);
-
 app.mount('#app');
-
 
 
 const app2 = createApp(test);
 app2.mount('#app-second');
-console.log('test');
 
 
 const app3 = createApp(test);
 app3.mount('#app-third');
-console.log('test');
 
+
+const burger = document.querySelector(`.burger`);
+const background = document.querySelector('.focusbackground');
+const menu = document.querySelector('.lg-menu');
+const loginForm = document.querySelector('.login-block');
+const loginButton = document.querySelector('.js-login');
+
+
+
+function disableBodyScroll() {
+    const element = document.querySelector("body");
+    element.classList.add("stop-scroll");
+}
+
+function enableBodyScroll() {
+    const element = document.querySelector("body");
+    element.classList.remove("stop-scroll");
+}
+
+function toggleMenu() {
+    background.classList.add("active");
+    menu.classList.add('active');
+    disableBodyScroll();
+
+}
+
+function closeAll() {
+    background.classList.remove(`active`);
+    menu.classList.remove('active');
+    loginForm.classList.remove(`active`);
+    enableBodyScroll();
+}
+function openLoginForm(){
+    background.classList.add("active");
+    loginForm.classList.add(`active`);
+    disableBodyScroll();
+}
+
+
+
+window.addEventListener('load', function () {
+
+    burger.addEventListener("click", toggleMenu);
+    background.addEventListener("click", closeAll);
+    loginButton.addEventListener(`click`, openLoginForm);
+
+
+});
